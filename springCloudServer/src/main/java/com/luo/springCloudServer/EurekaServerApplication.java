f84ea5e9b173c9b1c916be34269d788d7fec6772
xiBei_luo <1271826981@qq.com>
2018-12-27 09:01:54 +0800

    The springCloudServer

---
+++ springCloudServer/src/main/java/com/luo/springCloudServer/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.luo.springCloudServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * Created by Luowenlv on 2018/12/26,16:12
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
