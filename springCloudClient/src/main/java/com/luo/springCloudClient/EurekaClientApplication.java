552ee4913efbec9c294433ea38b240bb348d8491
xiBei_luo <1271826981@qq.com>
2018-12-27 09:03:24 +0800

    The springCloudClient

---
+++ springCloudClient/src/main/java/com/luo/springCloudClient/EurekaClientApplication.java
@@ -0,0 +1,31 @@
+package com.luo.springCloudClient;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Luowenlv on 2018/12/26,16:21
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/")
+    public String home() {
+        return "hello world from port " + port;
+    }
+
+}
