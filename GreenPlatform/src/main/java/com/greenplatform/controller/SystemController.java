6a7c1bc57bf60c8de58cb163f09e81a9839e6467
xiBei_luo <1271826981@qq.com>
2019-05-10 15:51:45 +0800

    system

--- GreenPlatform/src/main/java/com/greenplatform/controller/SystemController.java
+++ GreenPlatform/src/main/java/com/greenplatform/controller/SystemController.java
@@ -2,6 +2,7 @@ package com.greenplatform.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Luowenlv on 2019/5/7,11:25
@@ -9,4 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/system")
 public class SystemController {
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String index() {
+        return "system/index";
+    }
 }
