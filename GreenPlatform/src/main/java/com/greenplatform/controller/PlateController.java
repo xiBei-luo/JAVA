67c5e76aed5f760958c07d19aaa50b078cf09dea
xiBei_luo <1271826981@qq.com>
2019-05-12 21:49:23 +0800

    pageBar

--- GreenPlatform/src/main/java/com/greenplatform/controller/PlateController.java
+++ GreenPlatform/src/main/java/com/greenplatform/controller/PlateController.java
@@ -1,13 +1,18 @@
 package com.greenplatform.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.greenplatform.model.PlateUser;
 import com.greenplatform.model.base.ReturnModel;
 import com.greenplatform.service.PlateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by Luowenlv on 2019/5/7,11:25
@@ -20,8 +25,17 @@ public class PlateController {
     PlateService plateService;
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String index(PlateUser plateUser) {
-        System.out.println(plateService.selectPlateuser(plateUser));
+    public String index(PlateUser plateUser, Model model,Integer page) {
+        Integer limit = 30;
+        if ("".equals(page) || null == page){
+            page = 1;
+        }
+        PageHelper.startPage(page, limit);
+        List plateUserList = (List) (plateService.selectPlateuser(plateUser)).getObject();
+        PageInfo pageInfo=new PageInfo<>(plateUserList);
+        System.out.println(pageInfo);
+        model.addAttribute("plateUserList",plateUserList);
+        model.addAttribute("pageInfo",pageInfo);
         return "plate/index";
     }
 
---
+++ GreenPlatform/src/main/resources/static/plate/js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(".navList").find("li").each(function () {
+        var a = $(this).find("a:first")[0];
+        if ($(a).attr("href") === location.pathname) {
+            $(this).addClass("active");
+        } else {
+            $(this).removeClass("active");
+        }
+    });
+});
